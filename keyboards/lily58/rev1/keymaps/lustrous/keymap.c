1cd8419af5c61fab5849c80a15f58bbaa73c8dbf
Nipun Paradkar <nipunparadkar123@gmail.com>
2025-03-04 10:59:14 +0530

    Add forward delete
    Also updated the extreme left-thumb key to be Left GUI to match the one
    on the right.

--- keyboards/lily58/rev1/keymaps/lustrous/keymap.c
+++ keyboards/lily58/rev1/keymaps/lustrous/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * | Tab  |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  \   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
+ *                   | LGUI |Delete|LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_CAPS, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y, KC_U,    KC_I,    KC_O,    KC_P,       KC_EQL,
   KC_ESC,  HRM_G_A, HRM_A_S, HRM_C_D, HRM_S_F, KC_G,                   KC_H, HRM_S_J, HRM_C_K, HRM_A_L, HRM_G_SCLN, KC_QUOT,
   KC_TAB,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC, KC_RBRC, KC_N, KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    KC_BSLS,
-                       KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC,  KC_ENT,  MO(_RAISE), KC_BSPC, KC_RGUI
+                        KC_LGUI, KC_DEL, MO(_LOWER), KC_SPC,  KC_ENT, MO(_RAISE), KC_BSPC, KC_RGUI
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
