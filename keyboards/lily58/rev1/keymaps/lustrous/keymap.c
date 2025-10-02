9bbe810cc8ad803dd87514c1ac336a7f4fa61e91
Nipun Paradkar <nipunparadkar123@gmail.com>
2025-10-02 14:30:08 +0530

    Shift the left/right brackers to the right hand
    This is done so that it's easy to hit NeoVim's `[` and `]` bindings.
    Have moved the other keys around to compensate, but it might not be a
    desirable configuration.
    
    I might update the configuration related to the other keys based on how
    it feels to type.

--- keyboards/lily58/rev1/keymaps/lustrous/keymap.c
+++ keyboards/lily58/rev1/keymaps/lustrous/keymap.c
@@ -25,11 +25,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |Grave |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  -   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Caps |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  =   |
+ * | Caps |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  [   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | ESC  | LG&A | LA&S | LC&D | LS&F |   G  |-------.    ,-------|   H  | RS&J | RC&K | RA&L | RG&; |  '   |
- * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * | Tab  |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  \   |
+ * | ESC  | LG&A | LA&S | LC&D | LS&F |   G  |-------.    ,-------|   H  | RS&J | RC&K | RA&L | RG&; |  ]   |
+ * |------+------+------+------+------+------|   \   |    |    '  |------+------+------+------+------+------|
+ * | Tab  |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  =   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LGUI |Delete|LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -38,9 +38,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_QWERTY] = LAYOUT(
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                   KC_6, KC_7,    KC_8,    KC_9,    KC_0,       KC_MINS,
-  KC_CAPS, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y, KC_U,    KC_I,    KC_O,    KC_P,       KC_EQL,
-  KC_ESC,  HRM_G_A, HRM_A_S, HRM_C_D, HRM_S_F, KC_G,                   KC_H, HRM_S_J, HRM_C_K, HRM_A_L, HRM_G_SCLN, KC_QUOT,
-  KC_TAB,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC, KC_RBRC, KC_N, KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    KC_BSLS,
+  KC_CAPS, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y, KC_U,    KC_I,    KC_O,    KC_P,       KC_LBRC,
+  KC_ESC,  HRM_G_A, HRM_A_S, HRM_C_D, HRM_S_F, KC_G,                   KC_H, HRM_S_J, HRM_C_K, HRM_A_L, HRM_G_SCLN, KC_RBRC,
+  KC_TAB,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_BSLS, KC_QUOT, KC_N, KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    KC_EQL,
                         KC_LGUI, KC_DEL, MO(_LOWER), KC_SPC,  KC_ENT, MO(_RAISE), KC_BSPC, KC_RGUI
 ),
 /* LOWER
